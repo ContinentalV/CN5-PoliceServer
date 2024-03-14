7f60417f5717cdedab95b2afb237721125d198a5
Neo-Getsu <damien.devinfo@gmail.com>
2024-03-14 23:39:55 +0100

    troobleshooting

--- src/server.ts
+++ src/server.ts
@@ -7,8 +7,12 @@ import {logError, logInfo} from "./utils/functions";
 // Convertir explicitement PORT en number si nécessaire
 const PORT: number = parseInt(process.env.PORT || '8005', 10);
 
+console.log('avant server on')
 app.listen(PORT, async () => {
+    console.log("on est avec le server on la ")
+
     try {
+
         logInfo(`API Start on ${PORT}`);
         //await checkTables();
         // setInterval(monitorPerformance, 10000);
