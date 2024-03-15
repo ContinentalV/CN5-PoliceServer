696d32a18a434520d2522b829c4240f81deb26c0
Neo-Getsu <damien.devinfo@gmail.com>
2024-03-15 01:38:19 +0100

    troobleshooting fixing syntaxe

--- migrations/20240314031302-Services.js
+++ migrations/20240314031302-Services.js
@@ -2,7 +2,7 @@
 
 module.exports = {
     async up(queryInterface, Sequelize) {
-        await queryInterface.createTable('Services', {
+        await queryInterface.createTable('services', {
             id: {
                 type: Sequelize.INTEGER,
                 autoIncrement: true,
@@ -27,6 +27,6 @@ module.exports = {
     },
 
     async down(queryInterface, Sequelize) {
-        await queryInterface.dropTable('Services');
+        await queryInterface.dropTable('services');
     }
 };
