8426bed8d41e2ad231422ed684ee8de065a9baec
Neo-Getsu <damien.devinfo@gmail.com>
2024-03-15 15:55:18 +0100

    Adding Ecosystem pm2 for pm2 managing on prod

---
+++ ecosystem.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    apps: [{
+        name: 'policeServer',
+        script: 'build/server.js', // Chemin vers le script à exécuter
+        watch: true, // Activation de la surveillance des fichiers
+        ignore_watch: [
+            "./tmp",
+            "./styles",
+            './node_modules', // Ignorer le répertoire node_modules
+            './logs',         // Ignorer le répertoire des logs
+            '.env',         // Ignorer le fichier .env
+            "./build/utils"
+
+        ],
+        env: {
+            "NODE_ENV": "development",
+            // Définissez ici d'autres variables d'environnement pour le mode développement
+        },
+        env_production: {
+            "NODE_ENV": "production",
+            // Définissez ici d'autres variables d'environnement pour le mode production
+        }
+    }]
+};
