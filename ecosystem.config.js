64372d1c924953499c4adda3e397a70e721f2204
Neo-Getsu <damien.devinfo@gmail.com>
2024-03-17 22:39:31 +0100

    test script update clean++

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,24 +1,29 @@
+// ecosystem.config.js
 module.exports = {
-    apps: [{
-        name: 'policeServer',
-        script: 'build/server.js', // Chemin vers le script à exécuter
-        watch: true, // Activation de la surveillance des fichiers
-        ignore_watch: [
-            "./tmp",
-            "./styles",
-            './node_modules', // Ignorer le répertoire node_modules
-            './logs',         // Ignorer le répertoire des logs
-            '.env',         // Ignorer le fichier .env
-            "./build/utils"
-
-        ],
-        env: {
-            "NODE_ENV": "development",
-            // Définissez ici d'autres variables d'environnement pour le mode développement
+    apps: [
+        {
+            name: 'policeServer',
+            script: 'build/server.js',
+            instances: 1,
+            autorestart: true,
+            watch: false,
+            max_memory_restart: '1G',
+            env: {
+                NODE_ENV: 'development',
+            },
+            env_production: {
+                NODE_ENV: 'production',
+            },
+            exec_mode: 'fork',
+            // Hooks pour gérer les étapes d'installation et de construction
+            hooks: {
+                'pre-setup': 'npm install --production',
+                'post-setup': 'npm run build',
+                'pre-deploy': 'npm install --production',
+                'post-deploy': 'npm run build && pm2 startOrRestart ecosystem.config.js --env production',
+                'pre-restart': 'npm run build',
+                'post-restart': 'echo "Application restarted"',
+            },
         },
-        env_production: {
-            "NODE_ENV": "production",
-            // Définissez ici d'autres variables d'environnement pour le mode production
-        }
-    }]
+    ],
 };
